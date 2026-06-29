074a0a8e52127bd4eaea03ff1f1dafde51cc7e63
EstefanyMw <estefanylicinha@gmail.com>
2026-06-29 19:10:15 -0400

    feat: Implementação da questao 13

--- src/views/helpers/helpers.ts
+++ src/views/helpers/helpers.ts
@@ -20,7 +20,17 @@ function listNodeTechs(techs: Technology[]) {
     return `<ul>${list}</ul>`;
 }
 
+function formatDate(date: Date | string) {
+    if (!date) return '-';
+    return new Date(date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+}
+
 export default {
     listProfs,
-    listNodeTechs
-}
+    listNodeTechs,
+    formatDate,
+};
